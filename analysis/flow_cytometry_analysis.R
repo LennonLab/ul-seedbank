81646d6762966e496c6730ac2de8e38df1304366
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2021-05-05 16:07:43 -0600

    perform initial analysis of cell density over time

--- analysis/flow_cytometry_analysis.R
+++ analysis/flow_cytometry_analysis.R
@@ -5,8 +5,19 @@ library("flowStats")
 library("ggcyto")
 library("tidyverse")
 library("patchwork")
+library("lubridate")
+library("scales")
 
 design <- read.csv(file = "data/design.csv")
+
+env.data <- read.table("data/ul-seedbank.env.txt", sep="\t", header=TRUE)
+env.data$date <- as.Date(parse_date_time(env.data$date, "m d y"))
+env.data$doc[which(env.data$doc == "**")] <- NA
+env.data$doc <- as.numeric(as.character(env.data$doc))
+summary(env.data)
+design <- left_join(design, env.data[,c("sample.id", "date")])
+env.data <- env.data[-which(!(env.data$date %in% design$date)),]
+env.data
 
 frames <- lapply(dir("data/flow_cytometry/FCS/", full.names = TRUE), read.FCS)
 names(frames) <- sapply(frames, keyword, "TUBE NAME")
@@ -25,8 +36,16 @@ fsApply(fs, each_col, mean)
 
 cell_counts <- data.frame(sample.id = names(frames), cell_count = NA)
 
+# Controls
+# 1) Just Hoechst
+# 2) Just PY
+# 3) Just Live-Dead efluor stain
+# 6) Just Ecoli + beads
+# 7) Ecoli + beads + PY + Hoechst
+
 for(i in 1:length(fs)){
   f <- fs[[i]]
+  f
   ft <- transform(f, 
                   "Pacific Blue-H" = asinh(`Pacific Blue-H`), 
                   "APC-H" = asinh(`APC-H`))
@@ -51,22 +70,29 @@ for(i in 1:length(fs)){
 
 cell_count_dat <- cell_counts[-c(1:7),] %>%
   mutate(cell_count = as.numeric(cell_count),
-         sample.id = as.numeric(str_remove(sample.id, "UL_")))
+         sample.id = as.numeric(str_remove(sample.id, "UL_"))) %>% 
+  mutate(cell_density = cell_count / 50000 * 10^6 * 10^3) # cells/50000 bead counts * 10^6 beads/ml * 10^3 ml/L
 
 
-cell_plot <- env.dat %>% left_join(cell_count_dat, by = "sample.id") %>% 
+cell_plot <- env.data %>% left_join(cell_count_dat, by = "sample.id") %>% 
   filter(sample.id < 60) %>% 
-  ggplot(aes(x = sample.id, y = cell_count)) + 
+  ggplot(aes(x = date, y = cell_density)) + 
   geom_point() +
-  geom_line() +
-  theme_minimal()
+  theme_minimal() +
+  scale_y_log10(labels = label_scientific()) +
+  scale_x_date(breaks = "3 month") + 
+  labs(x = "",
+       y = expression(paste("Cell density (cells ", L^{-1},")")))
 
-temp_plot <- env.dat %>% left_join(cell_count_dat, by = "sample.id") %>% 
+temp_plot <- env.data %>% left_join(cell_count_dat, by = "sample.id") %>% 
   filter(sample.id < 60) %>% 
-  ggplot(aes(x = sample.id, y = temp)) + 
+  ggplot(aes(x = date, y = temp)) + 
   geom_point() +
-  geom_line() +
-  theme_minimal()
+  theme_minimal() +
+  scale_x_date(breaks = "3 month") + 
+  labs(x = "",
+       y = expression(paste("Temperature (",~degree,"C)")))
 
 cell_plot + temp_plot +
-  plot_layout(nrow = 2) 
+  plot_layout(nrow = 2, guides = "collect") +
+  ggsave("figures/cell_density.pdf", width = 6, height = 3/4*6)
