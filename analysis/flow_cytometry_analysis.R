e37c2c609340750ed50e6b40f7a5019a0d269055
Nathan Wisnoski <nathan.wisnoski@uwyo.edu>
2021-05-05 14:13:01 -0600

    perform gating along pac blue channel, filter and count cells in gate above threshold, and plot densities by sample

--- analysis/flow_cytometry_analysis.R
+++ analysis/flow_cytometry_analysis.R
@@ -1,7 +1,12 @@
 
 library("flowCore")
 library("flowViz")
+library("flowStats")
 library("ggcyto")
+library("tidyverse")
+library("patchwork")
+
+design <- read.csv(file = "data/design.csv")
 
 frames <- lapply(dir("data/flow_cytometry/FCS/", full.names = TRUE), read.FCS)
 names(frames) <- sapply(frames, keyword, "TUBE NAME")
@@ -10,13 +15,58 @@ sampleNames(fs)
 
 phenoData(fs)
 
-fsApply(fs, each_col, sum)
+fsApply(fs, each_col, mean)
 
-f <- fs[[10]]
-f
-autoplot(transform(f, 
-                   "Pacific Blue-H" = log1p(`Pacific Blue-H`), 
-                   "APC-H" = log1p(`APC-H`)),
-         "Pacific Blue-H", "APC-H")
+#f
+# autoplot(transform(f, 
+#                    "Pacific Blue-H" = asinh(`Pacific Blue-H`), 
+#                    "APC-H" = asinh(`APC-H`)),
+#          "Pacific Blue-H", "APC-H")
 
-?autoplot         
+cell_counts <- data.frame(sample.id = names(frames), cell_count = NA)
+
+for(i in 1:length(fs)){
+  f <- fs[[i]]
+  ft <- transform(f, 
+                  "Pacific Blue-H" = asinh(`Pacific Blue-H`), 
+                  "APC-H" = asinh(`APC-H`))
+  presel <- list("Pacific Blue-H"=c(9,Inf), "APC-H"=c(8,Inf))
+  
+  # lg <- lymphGate(ft,
+  #                 channels = c("Pacific Blue-H", "APC-H"), 
+  #                 preselection = presel, scale = 100,
+  #                 plot = F)
+  
+  rg <- rangeGate(ft, "Pacific Blue-H", alpha = "min",
+                  refLine = 5, plot = TRUE)
+  
+  xyplot(`Pacific Blue-H`~`APC-H`, ft, filter = rg)
+  
+  fr <- flowCore::filter(ft,rg)
+  count <- sum(fr@subSet)
+  id <- fr@frameId
+  cell_counts[i,] <- c(id, count)
+}
+
+
+cell_count_dat <- cell_counts[-c(1:7),] %>%
+  mutate(cell_count = as.numeric(cell_count),
+         sample.id = as.numeric(str_remove(sample.id, "UL_")))
+
+
+cell_plot <- env.dat %>% left_join(cell_count_dat, by = "sample.id") %>% 
+  filter(sample.id < 60) %>% 
+  ggplot(aes(x = sample.id, y = cell_count)) + 
+  geom_point() +
+  geom_line() +
+  theme_minimal()
+
+temp_plot <- env.dat %>% left_join(cell_count_dat, by = "sample.id") %>% 
+  filter(sample.id < 60) %>% 
+  ggplot(aes(x = sample.id, y = temp)) + 
+  geom_point() +
+  geom_line() +
+  theme_minimal()
+
+cell_plot + temp_plot +
+  plot_layout(nrow = 2) 
